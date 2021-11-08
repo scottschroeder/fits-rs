f7430209b034c0846d9524b41079a69e067db98b
Scott Schroeder <scottschroeder@sent.com>
2021-11-08 16:15:35 -0500

    added back in some tests for the parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -18,7 +18,9 @@ use nom::bytes::complete::take_while1;
 use nom::character::complete::multispace0;
 use nom::character::is_digit;
 use nom::combinator::eof;
+use nom::combinator::not;
 use nom::combinator::opt;
+use nom::combinator::peek;
 use nom::combinator::recognize;
 use nom::combinator::success;
 use nom::error::dbg_dmp;
@@ -186,10 +188,10 @@ fn value(input: &[u8]) -> IResult<&[u8], Value> {
     alt((
         character_string_value,
         logical_value,
-        floating_value,
         integer_value,
-        complex_floating_value,
+        floating_value,
         complex_integer_value,
+        complex_floating_value,
     ))(input)
 }
 
@@ -197,14 +199,13 @@ fn character_string_value(input: &[u8]) -> IResult<&[u8], Value> {
     // Constraint: the begin_quote and end_quote are not part of the
     // character string value but only serve as delimiters. Leading
     // spaces are significant; trailing spaces are not.
-    // TODO should we trim?
     // TODO is a double-single-quote "''" an escaped version of "'"?
     map(
         map_res(
             delimited(tag("'"), take_while(is_string_text_char), tag("'")),
             std::str::from_utf8,
         ),
-        Value::CharacterString,
+        |s| Value::CharacterString(s.trim_end()),
     )(input)
 }
 
@@ -225,10 +226,11 @@ fn integer_value(input: &[u8]) -> IResult<&[u8], Value> {
     map(integer, Value::Integer)(input)
 }
 
+// if an integer has a '.' after, we assume its a float and shouldn't parse
 fn integer(input: &[u8]) -> IResult<&[u8], i64> {
     map_res(
         map_res(
-            recognize(tuple((sign, take_while1(is_digit)))),
+            recognize(tuple((sign, take_while1(is_digit), peek(not(tag(".")))))),
             std::str::from_utf8,
         ),
         i64::from_str,
@@ -341,6 +343,18 @@ mod tests {
     use super::*;
 
     #[test]
+    fn header_should_parse_a_primary_header() {
+        let data =
+            include_bytes!("../../assets/images/k2-trappist1-unofficial-tpf-long-cadence.fits");
+        let (_, result) = header(&data[0..(10 * 2880)]).unwrap();
+        let expect = long_cadence_header();
+        for (idx, (r, e)) in result.into_iter().zip(expect).enumerate() {
+            assert_eq!(r, e, "{}", idx)
+        }
+        // assert_eq!(result[start..end], long_cadence_header()[start..end])
+    }
+
+    #[test]
     fn keyword_record_should_parse_a_keyword_record() {
         let data =
             "OBJECT  = 'EPIC 200164267'     / string version of target id                    "
@@ -377,6 +391,55 @@ mod tests {
     }
 
     #[test]
+    fn keyword_record_should_parse_a_keyword_record_without_a_comment() {
+        let data =
+            "KEPLERID=            200164267                                                  "
+                .as_bytes();
+
+        let (_, result) = keyword_record(data).unwrap();
+
+        assert_eq!(
+            result,
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::KEPLERID,
+                Value::Integer(200164267),
+                Option::None,
+            ))
+        )
+    }
+
+    #[allow(non_snake_case)]
+    #[test]
+    fn logical_constant_should_parse_an_uppercase_T_or_F() {
+        for (constant, boolean) in vec![("T", true), ("F", false)] {
+            let data = constant.as_bytes();
+
+            let (_, result) = logical_value(data).unwrap();
+            assert_eq!(result, Value::Logical(boolean))
+        }
+    }
+
+    #[test]
+    fn real_should_parse_an_floating_point_number() {
+        for (input, f) in vec![("1.0", 1f64), ("37.0", 37f64), ("51.0", 51f64)] {
+            let data = input.as_bytes();
+
+            let (_, result) = value(data).unwrap();
+            assert_eq!(result, Value::Real(f))
+        }
+    }
+
+    #[test]
+    fn integer_should_parse_an_integer() {
+        for (input, n) in vec![("1", 1i64), ("37", 37i64), ("51", 51i64)] {
+            let data = input.as_bytes();
+
+            let (_, result) = value(data).unwrap();
+            assert_eq!(result, Value::Integer(n))
+        }
+    }
+
+    #[test]
     fn parse_character_string_value() {
         let data = "'EPIC 200164267'".as_bytes();
         let (_, k) = character_string_value(data).unwrap();
@@ -398,6 +461,13 @@ mod tests {
     }
 
     #[test]
+    fn parse_integer_value() {
+        let data = "8".as_bytes();
+        let (_, k) = value(data).unwrap();
+        assert_eq!(k, Value::Integer(8))
+    }
+
+    #[test]
     fn parse_single_keywords() {
         let data = "OBJECT  ".as_bytes();
         let (_, k) = keyword_field(data).unwrap();
@@ -416,5 +486,301 @@ mod tests {
         let data = "/ string version of target id".as_bytes();
         let (_, k) = comment(data).unwrap();
         assert_eq!(k, "string version of target id")
+    }
+
+    fn long_cadence_header<'a>() -> Vec<KeywordRecord<'a>> {
+        vec![
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::SIMPLE,
+                Value::Logical(true),
+                Option::Some("conforms to FITS standards"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::BITPIX,
+                Value::Integer(8i64),
+                Option::Some("array data type"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::NAXIS,
+                Value::Integer(0i64),
+                Option::Some("number of array dimensions"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::EXTEND,
+                Value::Logical(true),
+                Option::Some("file contains extensions"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::NEXTEND,
+                Value::Integer(2i64),
+                Option::Some("number of standard extensions"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::EXTNAME,
+                Value::CharacterString("PRIMARY"),
+                Option::Some("name of extension"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::EXTVER,
+                Value::Integer(1i64),
+                Option::Some("extension version number (not format version)"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::ORIGIN,
+                Value::CharacterString("Unofficial data product"),
+                Option::Some("institution responsible for creating this"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::DATE,
+                Value::CharacterString("2017-03-08"),
+                Option::Some("file creation date."),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::CREATOR,
+                Value::CharacterString("kadenza"),
+                Option::Some("pipeline job and program u"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::PROCVER,
+                Value::CharacterString("2.1.dev"),
+                Option::Some("SW version"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::FILEVER,
+                Value::CharacterString("0.0"),
+                Option::Some("file format version"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::TIMVERSN,
+                Value::CharacterString(""),
+                Option::Some("OGIP memo number for file format"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::TELESCOP,
+                Value::CharacterString("Kepler"),
+                Option::Some("telescope"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::INSTRUME,
+                Value::CharacterString("Kepler Photometer"),
+                Option::Some("detector type"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::OBJECT,
+                Value::CharacterString("EPIC 200164267"),
+                Option::Some("string version of target id"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::KEPLERID,
+                Value::Integer(200164267i64),
+                Option::Some("unique Kepler target identifier"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::CHANNEL,
+                Value::Integer(68i64),
+                Option::Some("CCD channel"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::MODULE,
+                Value::Integer(19i64),
+                Option::Some("CCD module"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::OUTPUT,
+                Value::Integer(4i64),
+                Option::Some("CCD output"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::CAMPAIGN,
+                Value::CharacterString(""),
+                Option::Some("Observing campaign number"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::DATA_REL,
+                Value::CharacterString(""),
+                Option::Some("data release version number"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::OBSMODE,
+                Value::CharacterString("long cadence"),
+                Option::Some("observing mode"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::MISSION,
+                Value::CharacterString("K2"),
+                Option::Some("Mission name"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::TTABLEID,
+                Value::CharacterString(""),
+                Option::Some("target table id"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::RADESYS,
+                Value::CharacterString("ICRS"),
+                Option::Some("reference frame of celestial coordinates"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::RA_OBJ,
+                Value::CharacterString(""),
+                Option::Some("[deg] right ascension"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::DEC_OBJ,
+                Value::CharacterString(""),
+                Option::Some("[deg] declination"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::EQUINOX,
+                Value::Real(2000.0f64),
+                Option::Some("equinox of celestial coordinate system"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::PMRA,
+                Value::Undefined,
+                Option::Some("[arcsec/yr] RA proper motion"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::PMDEC,
+                Value::Undefined,
+                Option::Some("[arcsec/yr] Dec proper motion"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::PMTOTAL,
+                Value::Undefined,
+                Option::Some("[arcsec/yr] total proper motion"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::PARALLAX,
+                Value::Undefined,
+                Option::Some("[arcsec] parallax"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::GLON,
+                Value::Undefined,
+                Option::Some("[deg] galactic longitude"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::GLAT,
+                Value::Undefined,
+                Option::Some("[deg] galactic latitude"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::GMAG,
+                Value::Undefined,
+                Option::Some("[mag] SDSS g band magnitude"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::RMAG,
+                Value::Undefined,
+                Option::Some("[mag] SDSS r band magnitude"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::IMAG,
+                Value::Undefined,
+                Option::Some("[mag] SDSS i band magnitude"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::ZMAG,
+                Value::Undefined,
+                Option::Some("[mag] SDSS z band magnitude"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::JMAG,
+                Value::Undefined,
+                Option::Some("[mag] J band magnitude from 2MASS"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::HMAG,
+                Value::Undefined,
+                Option::Some("[mag] H band magnitude from 2MASS"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::KMAG,
+                Value::Undefined,
+                Option::Some("[mag] K band magnitude from 2MASS"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::KEPMAG,
+                Value::Undefined,
+                Option::Some("[mag] Kepler magnitude (Kp)"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::GRCOLOR,
+                Value::Undefined,
+                Option::Some("[mag] (g-r) color, SDSS bands"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::JKCOLOR,
+                Value::Undefined,
+                Option::Some("[mag] (J-K) color, 2MASS bands"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::GKCOLOR,
+                Value::Undefined,
+                Option::Some("[mag] (g-K) color, SDSS g - 2MASS K"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::TEFF,
+                Value::Undefined,
+                Option::Some("[K] Effective temperature"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::LOGG,
+                Value::Undefined,
+                Option::Some("[cm/s2] log10 surface gravity"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::FEH,
+                Value::Undefined,
+                Option::Some("[log10([Fe/H])]  metallicity"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::EBMINUSV,
+                Value::Undefined,
+                Option::Some("[mag] E(B-V) reddening"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::AV,
+                Value::Undefined,
+                Option::Some("[mag] A_v extinction"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::RADIUS,
+                Value::Undefined,
+                Option::Some("[solar radii] stellar radius"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::TMINDEX,
+                Value::Undefined,
+                Option::Some("unique 2MASS catalog ID"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::CHECKSUM,
+                Value::CharacterString("7k7A7h637h697h69"),
+                Option::Some("HDU checksum updated 2017-03-08T02:47:56"),
+            )),
+            KeywordRecord::ValueRecord(ValueRecord::new(
+                Keyword::DATASUM,
+                Value::CharacterString("0"),
+                Option::Some("data unit checksum updated 2017-03-08T02:47:56"),
+            )),
+            KeywordRecord::EndRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+            KeywordRecord::BlankRecord,
+        ]
     }
 }
